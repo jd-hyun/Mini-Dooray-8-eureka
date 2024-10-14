ddb1fcd70dc8db63fea9de1ca31599a616e3ca0a
jd-hyun <wjdehdgus3540@naver.com>
2024-10-15 01:56:58 +0900

    과제 제출

---
+++ src/test/java/com/nhnacademy/minidooray8eureka/MiniDooray8EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidooray8eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniDooray8EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
